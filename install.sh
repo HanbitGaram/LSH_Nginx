5963351a413421198a30778589a9bdbb054b1550
한빛가람 <18398825+HanbitGaram@users.noreply.github.com>
2020-05-23 13:46:16 +0900

    * 일부 기능 추가 및 수정
    *) 추가 : 데비안 패키지 libssl-dev 추가 (nginx 내에서 SSL을 지원하기 위함)
    *) 추가 : OpenSSL 모듈 다운로드 (Alpha 3.x는 작동하지 않으므로 1.x의 마지막 대역)
    *) 추가 : headers_more_nginx 모듈 다운로드 (헤더를 숨기기에 용이함)
    *) 수정 : 헤더명에 버전이 노출되는 문제를 방지하고자, 서버 헤더를 HostLSH Blank로 변경.

--- install.sh
+++ install.sh
@@ -4,19 +4,36 @@ apt-get -y update
 apt-get -y upgrade
 apt -y update
 apt -y upgrade
-apt-get -y install unzip gcc g++ openssl zlib1g-dev libpcre3 libpcre3-dev make git
+apt-get -y install unzip gcc g++ openssl zlib1g-dev libpcre3 libpcre3-dev make git libssl-dev
 
 mkdir /opt/
 git clone https://github.com/HanbitGaram/LSH_Nginx.git /opt/LSH_nginx
+cd /opt/LSH_nginx
 git pull
 
-cd /opt/LSH_nginx
+rm -rf /opt/LSH_nginx/openssl_111g
+wget https://www.openssl.org/source/openssl-1.1.1g.tar.gz -O ./openssl_111g.tar.gz
+tar -xvzpf openssl_111g.tar.gz
+mv openssl-1.1.1g openssl_111g
+
+git clone https://github.com/openresty/headers-more-nginx-module.git
+cd headers-more-nginx-module
+git pull
+
+cd ..
 
 ./configure \
 --prefix=/usr/local/nginx \
 --user=www-data \
 --group=www-data \
---with-openssl=/usr/bin
+--with-openssl=/opt/LSH_nginx/openssl_111g \
+--with-http_ssl_module \
+--with-http_realip_module \
+--with-http_stub_status_module \
+--with-http_realip_module \
+--with-ipv6 \
+--add-module=/opt/LSH_nginx/headers-more-nginx-module
+
 
 make -j 4
 make install -j 4
